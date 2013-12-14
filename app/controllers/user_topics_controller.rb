f305b8715dcdbf98a461050579b405ceb4f6ed09
Apprentice <apprentice@dbc11.local>
2013-12-14 13:39:33 -0800

    - Add user/peer progress graph. - Add history to user model/migration.

--- app/controllers/user_topics_controller.rb
+++ app/controllers/user_topics_controller.rb
@@ -1,15 +1,35 @@
 class UserTopicsController < ApplicationController
   def update
-    p "*" * 50
-    p params
     @user_topic = UserTopic.find(params[:id])
     topic_belongs_to_current_user = false
     current_user.user_topics.each do |user_topic|
       topic_belongs_to_current_user = true if user_topic.id == @user_topic.id
     end
     if topic_belongs_to_current_user
+      user = current_user
       @user_topic.rating = params[:new_rating].to_i
       @user_topic.save
+      history = @user_topic.user.history
+      list_being_modified_id = List.find(@user_topic.topic.list_id).id
+      list_being_modified = List.find(@user_topic.topic.list_id).name
+      column_being_modified = history[0].index(list_being_modified)
+      last_entry = history[-1]
+      todays_date = Time.new.to_date.to_s
+      user_topics_in_list_being_modified = []
+      UserTopic.all.each do |user_topic|
+        user_topics_in_list_being_modified << user_topic if user_topic.user_id == user.id && user_topic.topic.list_id == list_being_modified_id
+      end
+      list_ratings = user_topics_in_list_being_modified.inject(0) {|sum, user_topic| sum + user_topic.rating }
+      if last_entry[0] == todays_date
+        last_entry[column_being_modified] = list_ratings
+      else
+        new_entry = last_entry.dup
+        new_entry[0] = todays_date
+        new_entry[column_being_modified] = list_ratings
+        history.push(new_entry)
+      end
+      user.history = history
+      user.save
     end
     @user = User.find(params[:peer_id])
     @user_topics = @user.user_topics.order('created_at ASC')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,47 @@
+require 'set'
 class User < ActiveRecord::Base
   include Clearance::User
   attr_accessible :firstname, :lastname, :username, :email, :password
   attr_reader :admin
   has_many :user_topics
   has_many :resources
+  serialize :history
   after_create :create_topics
+  after_create :create_history
 
 
-  protected
+
+
+  # protected
   def create_topics
     Topic.all.each do |topic|
       UserTopic.create(user_id: self.id, topic_id: topic.id, rating: 0)
     end
   end
+
+  def create_history
+    user_topics = self.user_topics.order('created_at ASC')
+
+    user_history = [['Date']]
+
+    list_ids = Set.new
+
+    user_topics.each do |user_topic|
+      list_ids.add(user_topic.topic.list_id)
+    end
+
+    list_ids.each do |list_id|
+      this_list = List.find(list_id)
+      user_history[0] << this_list.name
+    end
+
+    row_length = user_history[0].length
+    next_row = Array.new(row_length, 0)
+    next_row[0] = Time.new.to_date.to_s
+    user_history.push(next_row)
+
+    self.history = user_history
+    self.save
+  end
+
 end
