65ad0b3d5c8158f115ccc70994ad9b99a9dd64a8
Marion Knight <knight499@gmail.com>
2013-12-13 21:05:13 -0800

    Add skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Progressy::Application.config.secret_token = 'b65e8ffd6e208de722f485c25e16f6537bfdb7de4de4b8f60450983198155ca0405b8c81df04bd50c5c2be51d8083dbeeb69cd137ea30e997c5c1f9680cd06f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Progressy::Application.config.session_store :cookie_store, key: '_progressy_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Progressy::Application.config.session_store :active_record_store
