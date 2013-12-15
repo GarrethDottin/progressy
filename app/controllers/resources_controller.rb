1768d13426ac21a9b8756cb19c53c38e3f6cf387
Louie Perez <louie.perez3@gmail.com>
2013-12-14 16:35:14 -0800

    Resource Ready for AJAX

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,9 @@
 class ResourcesController < ApplicationController
   def create
     resource = Resource.create params[:resource]
+    if xhr.request?
+      render partial: 'topics/topic', locals: {topic: resource.topic_id}, layout: false
+    end
     redirect_to topic_path(resource.topic.id)
   end
 end
