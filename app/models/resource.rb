e9a2c96666c0ac58bb9e2115327b773a5822741d
Apprentice <apprentice@dbc11.local>
2013-12-14 18:30:50 -0800

    Fix ajax problems with lists page.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,5 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :content, :topic_id
+  attr_accessible :content, :topic_id, :user_id
   validates :content, presence:  true
   belongs_to :topic
   belongs_to :user
--- db/migrate/20131214055404_create_resources.rb
+++ db/migrate/20131214055404_create_resources.rb
@@ -3,6 +3,7 @@ class CreateResources < ActiveRecord::Migration
     create_table :resources do |t|
       t.string :content
       t.belongs_to :list
+      t.belongs_to :user
       t.timestamps
     end
   end
